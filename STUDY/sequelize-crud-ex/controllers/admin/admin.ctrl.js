a2355efb5ed5cc26bcc4d60e19b14c90725f97c2
백다미 <100damii@naver.com>
2021-08-25 20:02:24 +0900

    UPDATE, DELETE

--- STUDY/sequelize-crud-ex/controllers/admin/admin.ctrl.js
+++ STUDY/sequelize-crud-ex/controllers/admin/admin.ctrl.js
@@ -27,6 +27,16 @@ exports.post_products_write = (req, res) => {
     });
 }
 
+exports.get_products_delete = (req, res) => {
+    models.Products.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(() => {
+        res.redirect('/admin/products');
+    });
+};
+
 // exports.get_products_detail = (req, res) => {
 //     //req.params.id
 //     models.Products.findByPk(req.params.id).then((product) => {
@@ -39,3 +49,27 @@ exports.get_products_detail = (req, res) => {
         res.render('admin/detail.html', { product: product });
     });
 };
+
+exports.get_products_edit = (req, res) => {
+    //기존에 폼에 value안에 값을 셋팅하기 위해 만든다.
+    models.Products.findByPk(req.params.id).then((product) => {
+        res.render('admin/write.html', { product: product });
+    });
+};
+
+exports.post_products_edit = (req, res) => {
+
+    models.Products.update(
+        {
+            name: req.body.name,
+            price: req.body.price,
+            description: req.body.description
+        },
+        {
+            where: { id: req.params.id }
+        }
+    ).then(() => {
+        res.redirect('/admin/products/detail/' + req.params.id);
+    });
+
+}
--- STUDY/sequelize-crud-ex/controllers/admin/index.js
+++ STUDY/sequelize-crud-ex/controllers/admin/index.js
@@ -10,6 +10,12 @@ router.post('/products/write', ctrl.post_products_write);
 
 router.get('/products/detail/:id', ctrl.get_products_detail);
 
+router.get('/products/edit/:id', ctrl.get_products_edit);
+
+router.post('/products/edit/:id', ctrl.post_products_edit);
+
+router.get('products/delete/:id', ctrl.get_products_delete);
+
 module.exports = router;
 
 
